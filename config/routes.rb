042e28315a0b6b1b856dd53b2097875d60ecc199
kiota-rita <rita-kiota@hotmail.com>
2022-08-14 19:05:32 -0300

    add static pages sobre and contato

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "static_pages#index" #controller+action
+  
+  get "sobre", to: "static_pages#sobre"
+
+  get "contato", to: "static_pages#contato"
+  
 end
